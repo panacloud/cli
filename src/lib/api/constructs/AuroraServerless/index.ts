773c5e89c01d688d74182064e2dba72a0e1d47b2
Muh-Hasan <muh.hasan.git@gmail.com>
2021-09-02 23:21:27 +0500

    AuroraServerless construct added (#8)

---
+++ src/lib/api/constructs/AuroraServerless/index.ts
@@ -0,0 +1,251 @@
+import { CodeMaker } from "codemaker";
+import { TypeScriptWriter } from "../../../../utils/typescriptWriter";
+let maker = new CodeMaker();
+
+export class AuroraServerless extends CodeMaker {
+  public initializeAuroraCluster(apiName: string, vpcName: string) {
+    const ts = new TypeScriptWriter(maker);
+    ts.writeVariableDeclaration(
+      {
+        name: `${apiName}_db`,
+        typeName: "",
+        initializer: () => {
+          this.line(`new rds.ServerlessCluster(this, "${apiName}DB", {
+            vpc: ${vpcName},
+            engine: rds.DatabaseClusterEngine.auroraMysql({
+              version: rds.AuroraMysqlEngineVersion.VER_5_7_12,
+            }),
+            scaling: {
+              autoPause: Duration.minutes(10), 
+              minCapacity: rds.AuroraCapacityUnit.ACU_8, 
+              maxCapacity: rds.AuroraCapacityUnit.ACU_32,
+            },
+            deletionProtection: false,
+            defaultDatabaseName: "${apiName}DB",
+          });`);
+        },
+      },
+      "const"
+    );
+  }
+
+  public connectionsAllowFromAnyIpv4(sourceName: string) {
+    this.line(
+      `${sourceName}.connections.allowFromAnyIpv4(ec2.Port.tcp(3306));`
+    );
+  }
+
+  public route_tableIdentifier(state: string) {
+    this.line(`const ${state}RouteTables = [
+    ${state}_subnets[0].routeTable,
+    ${state}_subnets[1].routeTable,
+  ];`);
+  }
+
+  public initializeTestForEC2Vpc() {
+    this.line(`expect(stack).toHaveResource('AWS::EC2::VPC', {
+      CidrBlock: '10.0.0.0/16',
+      EnableDnsHostnames: true,
+      EnableDnsSupport: true,
+      InstanceTenancy: 'default',
+    });
+   `);
+  }
+
+  public initializeTestForSubnet(
+    apiName: string,
+    cidrBlock: string,
+    fNum: number,
+    mapPublicIpOnLaunch: boolean,
+    state: string,
+    stateNum: string
+  ) {
+    this.line(`expect(stack).toHaveResource('AWS::EC2::Subnet', {
+      CidrBlock: '${cidrBlock}',
+      VpcId: {
+        Ref: stack.getLogicalId(AuroraDbConstruct_stack.vpcRef.node.defaultChild as cdk.CfnElement),
+      },
+      AvailabilityZone: {
+        'Fn::Select': [
+          ${fNum},
+          {
+            'Fn::GetAZs': '',
+          },
+        ],
+      },
+      MapPublicIpOnLaunch: ${mapPublicIpOnLaunch},
+      Tags: [
+        {
+          Key: 'aws-cdk:subnet-name',
+          Value: '${state}',
+        },
+        {
+          Key: 'aws-cdk:subnet-type',
+          Value: '${state}',
+        },
+        {
+          Key: 'Name',
+          Value: 'Default/AuroraDbConstructTest/${apiName}Vpc/${state}Subnet${stateNum}',
+        },
+      ],
+    });`);
+  }
+
+  public initializeTestForRouteTable(
+    apiName: string,
+    state: string,
+    stateNum: string
+  ) {
+    this.line(`expect(stack).toHaveResource('AWS::EC2::RouteTable', {
+      VpcId: {
+        Ref: stack.getLogicalId(AuroraDbConstruct_stack.vpcRef.node.defaultChild as cdk.CfnElement),
+      },
+      Tags: [
+        {
+          Key: 'Name',
+          Value: 'Default/AuroraDbConstructTest/${apiName}Vpc/${state}Subnet${stateNum}',
+        },
+      ],
+    });
+  `);
+  }
+
+  public initializeTestForSubnetRouteTableAssociation(
+    routeTableState: string,
+    routeTableNum: number,
+    routeTable: string,
+    routeTableId: string,
+    subnet: string,
+    subnetState: number
+  ) {
+    this
+      .line(`expect(stack).toHaveResource('AWS::EC2::SubnetRouteTableAssociation', {
+      RouteTableId: stack.resolve(${routeTableState}[${routeTableNum}].${routeTable}${routeTableId}),
+      SubnetId: {
+        Ref: stack.getLogicalId(
+          ${subnet}[${subnetState}].node.defaultChild as cdk.CfnElement
+        ),
+      },
+    });`);
+  }
+
+  public initializeTestForSecurityGroup() {
+    this.line(`expect(stack).toHaveResource('AWS::EC2::SecurityGroup', {
+      GroupDescription: 'RDS security group',
+      SecurityGroupEgress: [
+        {
+          CidrIp: '0.0.0.0/0',
+          Description: 'Allow all outbound traffic by default',
+          IpProtocol: '-1',
+        },
+      ],
+      SecurityGroupIngress: [
+        {
+          CidrIp: '0.0.0.0/0',
+          Description: 'from 0.0.0.0/0:3306',
+          FromPort: 3306,
+          IpProtocol: 'tcp',
+          ToPort: 3306,
+        },
+      ],
+      VpcId: {
+        Ref: stack.getLogicalId(AuroraDbConstruct_stack.vpcRef.node.defaultChild as cdk.CfnElement),
+      },
+    });
+  `);
+  }
+
+  public initializeTestForRoute(
+    routeTableState: string,
+    routeTableNum: number,
+    gatewatIdType: string,
+    gatewayState: string
+  ) {
+    this.line(`expect(stack).toHaveResource('AWS::EC2::Route', {
+      RouteTableId: stack.resolve(${routeTableState}[${routeTableNum}].routeTableId),
+      DestinationCidrBlock: '0.0.0.0/0',
+      ${gatewatIdType}: {
+        Ref: stack.getLogicalId(${gatewayState}[0] as cdk.CfnElement),
+      },
+    });`);
+  }
+
+  public initializeTestForEIP(apiName: string, stateNum: string) {
+    this.line(`expect(stack).toHaveResource('AWS::EC2::EIP', {
+      Domain: 'vpc',
+      Tags: [
+        {
+          Key: 'Name',
+          Value: 'Default/AuroraDbConstructTest/${apiName}Vpc/PublicSubnet${stateNum}',
+        },
+      ],
+    });`);
+  }
+
+  public initializeTestForNatGateway(
+    apiName: string,
+    subentNum: number,
+    eipNum: string,
+    stateNum: string
+  ) {
+    this.line(`expect(stack).toHaveResource('AWS::EC2::NatGateway', {
+      SubnetId: {
+        Ref: stack.getLogicalId(
+          public_subnets[${subentNum}].node.defaultChild as cdk.CfnElement
+        ),
+      },
+      AllocationId: {
+        'Fn::GetAtt': [
+          stack.getLogicalId(eip${eipNum}[0] as cdk.CfnElement),
+          'AllocationId',
+        ],
+      },
+      Tags: [
+        {
+          Key: 'Name',
+          Value: 'Default/AuroraDbConstructTest/${apiName}Vpc/PublicSubnet${stateNum}',
+        },
+      ],
+    });`);
+  }
+
+  public initializeTestForDBSubnetGroup(apiName: string) {
+    this.line(`expect(stack).toHaveResource('AWS::RDS::DBSubnetGroup', {
+      DBSubnetGroupDescription: 'Subnets for ${apiName}DB database',
+      SubnetIds: subnetRefArray,
+    });`);
+  }
+
+  public ininitializeTestForRole() {
+    this.line(`expect(stack).toHaveResource('AWS::IAM::Role', {
+      AssumeRolePolicyDocument: {
+        Statement: [
+          {
+            Action: 'sts:AssumeRole',
+            Effect: 'Allow',
+            Principal: {
+              Service: 'lambda.amazonaws.com',
+            },
+          },
+        ],
+        Version: '2012-10-17',
+      },
+    });`);
+  }
+
+  public initializeTestForVPCGatewayAttachment() {
+    this.line(`expect(stack).toHaveResource('AWS::EC2::VPCGatewayAttachment', {
+      VpcId: {
+        Ref: stack.getLogicalId(AuroraDbConstruct_stack.vpcRef.node.defaultChild as cdk.CfnElement),
+      },
+      InternetGatewayId: {
+        Ref: stack.getLogicalId(internetGateway[0] as cdk.CfnElement),
+      },
+    });
+  `);
+  }
+
+  public initializeTestForCountResources(service: string, count: number) {
+    this.line(`expect(stack).toCountResources('${service}', ${count});`);
+  }
+}
