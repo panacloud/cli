e8dbd3fd92904809d3a9159d817c9051018b056e
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-09-07 19:42:24 +0500

    added lambda functions for open api (#8)

--- src/lib/api/generators/Lambda/lambdaHandlers.ts
+++ src/lib/api/generators/Lambda/lambdaHandlers.ts
@@ -1,7 +1,7 @@
 import { CodeMaker } from "codemaker";
 import { APITYPE, Config, LAMBDASTYLE } from "../../../../utils/constants";
 import { LambdaFunction } from "../../constructs/Lambda/lambdaFunction";
-// const SwaggerParser = require("@apidevtools/swagger-parser");
+const SwaggerParser = require("@apidevtools/swagger-parser");
 
 type StackBuilderProps = {
   config: Config;
@@ -18,26 +18,28 @@ class LambdaHandlers {
     this.code = new CodeMaker();
   }
 
-  async lambdaHandlers() {
-    const { api: { lambdaStyle, apiType }} = this.config;
+  async lambdaHandler() {
+    const {
+      api: { lambdaStyle, apiType },
+    } = this.config;
     const { type } = this.config;
 
     if (apiType === APITYPE.graphql) {
       if (lambdaStyle === LAMBDASTYLE.single) {
         if (type?.Query) {
-          Object.keys(type.Query).forEach(async(key) => {
-            this.outputFile = `${key}.ts`
-            this.code.openFile(this.outputFile)
+          Object.keys(type.Query).forEach(async (key) => {
+            this.outputFile = `${key}.ts`;
+            this.code.openFile(this.outputFile);
             const lambda = new LambdaFunction(this.code);
-            lambda.helloWorldFunction(key); 
+            lambda.helloWorldFunction(key);
             this.code.closeFile(this.outputFile);
             await this.code.save(this.outputDir);
-        });
+          });
         }
         if (type?.Mutation) {
-          Object.keys(type.Mutation).forEach(async(key) => {
-            this.outputFile = `${key}.ts`
-            this.code.openFile(this.outputFile)
+          Object.keys(type.Mutation).forEach(async (key) => {
+            this.outputFile = `${key}.ts`;
+            this.code.openFile(this.outputFile);
             const lambda = new LambdaFunction(this.code);
             lambda.helloWorldFunction(key);
             this.code.closeFile(this.outputFile);
@@ -45,26 +47,26 @@ class LambdaHandlers {
           });
         }
       }
-    } // }else {
-    //     SwaggerParser.validate(model.openApiDef, (err: any, api: any) => {
-    //       if (err) {
-    //         console.error(err);
-    //       } else {
-    //         Object.keys(api.paths).forEach((path) => {
-    //           for (var methodName in api.paths[`${path}`]) {
-    //             let lambdaFunctionFile =
-    //               api.paths[`${path}`][`${methodName}`][`operationId`];
-    //             // Generator.generate(
-    //             //   {
-    //             //     outputFile: `${PATH.lambda}${lambdaFunctionFile}.ts`,
-    //             //   },
-    //             //   (writer: TextWriter) => {
-    //                 const lambda = new LambdaFunction(this.code);
-    //                 lambda.helloWorldFunction(lambdaFunctionFile);
-    //             //   }
-    //             // );
-    //           }
-    //         });
+    } else {
+      SwaggerParser.validate("./nexmo.json", (err: any, api: any) => {
+        if (err) {
+          console.log(err);
+        } else {
+          Object.keys(api.paths).forEach(async (path) => {
+            for (var methodName in api.paths[`${path}`]) {
+              let lambdaFunctionFile =
+                api.paths[`${path}`][`${methodName}`][`operationId`];
+              this.outputFile = `${lambdaFunctionFile}.ts`;
+              this.code.openFile(this.outputFile);
+              const lambda = new LambdaFunction(this.code);
+              lambda.helloWorldFunction(lambdaFunctionFile);
+              this.code.closeFile(this.outputFile);
+              await this.code.save(this.outputDir);
+            }
+          });
+        }
+      });
+    }
   }
 }
 
@@ -72,5 +74,5 @@ export const lambdaHandlers = async (
   props: StackBuilderProps
 ): Promise<void> => {
   const builder = new LambdaHandlers(props);
-  await builder.lambdaHandlers();
+  await builder.lambdaHandler();
 };
