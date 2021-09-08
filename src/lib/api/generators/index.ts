164642a39dd1f5ee10bd67d83a00c6ca68c14528
Muh-Hasan <muh.hasan.git@gmail.com>
2021-09-08 20:58:22 +0500

    bin construct fixed (#8)

--- src/lib/api/generators/index.ts
+++ src/lib/api/generators/index.ts
@@ -1,14 +1,19 @@
 import { ApiModel, APITYPE } from "../../../utils/constants";
 import { ApiGatewayConstruct } from "./ApiGateway";
 import { AppsyncApiConstruct } from "./Appsync";
+import { CdkAppClass } from "./bin";
 
 export const generator = async (config: ApiModel) => {
   console.log(config);
 
+  // bin file
+  CdkAppClass({config})
+
+  // Appsync or Apigateway 
   if (config.api.apiType === APITYPE.graphql) {
     AppsyncApiConstruct({ config });
   } else if (config.api.apiType === APITYPE.rest) {
     ApiGatewayConstruct({ config });
   }
-  
+
 };
