0a3a9fdf0b26de1314edbc0c9f67531280bb6030
Muh-Hasan <muh.hasan.git@gmail.com>
2021-10-07 16:15:10 +0500

    fixed buildSchematoTypescript (#32)

--- src/lib/api/buildSchemaToTypescript.ts
+++ src/lib/api/buildSchemaToTypescript.ts
@@ -4,7 +4,7 @@ export const buildSchemaToTypescript = (gqlSchema: any) => {
   let includeDeprecatedFields = false;
 
   let collectionsObject: {
-    fields: { [k: string]: [{ arguments: any; response: any }] };
+    fields: { [k: string]: [{ arguments?: any; response: any }] };
   } = {
     fields: {},
   };
@@ -17,15 +17,32 @@ export const buildSchemaToTypescript = (gqlSchema: any) => {
     Object.keys(obj).forEach((type: string) => {
       const field = gqlSchema.getType(description).getFields()[type];
       if (includeDeprecatedFields || !field.isDeprecated) {
-        const field = gqlSchema.getType(description).getFields()[type];
         const responseTypeName = field.type.inspect().replace(/[[\]!]/g, "");
-        collectionsObject.fields[type] = [{ arguments: {}, response: {} }];
-        typeStr += `${type}: [{ arguments: ${description}${upperFirst(
-          type
-        )}Args; response: ${responseTypeName} }];\n`;
 
-        allImports.push(responseTypeName);
-        allImports.push(`${description}${upperFirst(type)}Args`);
+        collectionsObject.fields[type] =
+          field.args.length > 0
+            ? [{ arguments: {}, response: {} }]
+            : [{ response: {} }];
+
+        field.args.length > 0
+          ? (typeStr += `${type}: [{arguments: ${description}${upperFirst(
+              type
+            )}Args; response: ${responseTypeName} }];\n`)
+          : `${type}: [{ response: ${responseTypeName} }];\n`;
+
+        if (
+          (responseTypeName === "String" ||
+            responseTypeName === "Int" ||
+            responseTypeName === "ID") &&
+          field.args.length !== 0
+        ) {
+          allImports.push(`${description}${upperFirst(type)}Args`);
+        } else if (field.args.length > 0) {
+          allImports.push(responseTypeName);
+          allImports.push(`${description}${upperFirst(type)}Args`);
+        } else {
+          return;
+        }
       }
     });
   };
