965d0fe01d124294eb861c844a33f323d6d2f3f2
Muh-Hasan <muh.hasan.git@gmail.com>
2021-10-14 16:41:53 +0500

    fixed:  model (#51)

--- src/lib/api/functions/DefineYourOwnApi/index.ts
+++ src/lib/api/functions/DefineYourOwnApi/index.ts
@@ -12,7 +12,11 @@ import {
   PanacloudconfigFile,
 } from "../../../../utils/constants";
 import { generator } from "../../generators";
-import { introspectionFromSchema, buildSchema, GraphQLObjectType } from "graphql";
+import {
+  introspectionFromSchema,
+  buildSchema,
+  GraphQLObjectType,
+} from "graphql";
 import { buildSchemaToTypescript } from "../../buildSchemaToTypescript";
 import { EliminateScalarTypes, ScalarAndEnumKindFinder } from "../../helpers";
 import { CreateAspects } from "../../generators/Aspects";
@@ -26,7 +30,9 @@ const snakeCase = require("lodash/snakeCase");
 async function defineYourOwnApi(config: Config, templateDir: string) {
   const { api_token, entityId } = config;
 
-  const {api: { schemaPath, apiType,nestedResolver } } = config;
+  const {
+    api: { schemaPath, apiType, nestedResolver },
+  } = config;
 
   const workingDir = snakeCase(path.basename(process.cwd()));
 
@@ -55,16 +61,12 @@ async function defineYourOwnApi(config: Config, templateDir: string) {
     }
   });
 
-  
-
   // Updating fileName
   const stackPackageJson = JSON.parse(
     fs.readFileSync(`${templateDir}/package.json`)
   );
 
-
   const cdkJson = JSON.parse(fs.readFileSync(`${templateDir}/cdk.json`));
-
 
   stackPackageJson.bin = `bin/${workingDir}.js`;
   stackPackageJson.name = workingDir;
@@ -81,7 +83,6 @@ async function defineYourOwnApi(config: Config, templateDir: string) {
       }
     }
   );
-
 
   await fs.writeFileSync(
     `./cdk.json`,
@@ -159,15 +160,17 @@ async function defineYourOwnApi(config: Config, templateDir: string) {
     );
 
     const gqlSchema = buildSchema(`${scalars}\n${directives}\n${schema}`);
-    const schemaJson = introspectionFromSchema(gqlSchema)
+    const schemaJson = introspectionFromSchema(gqlSchema);
     let nestedResolverTypes: { [key: string]: string[] } = {};
     let schemaTypes: string[] = [];
 
-    if(nestedResolver){
+    if (nestedResolver) {
       schemaJson.__schema.types.map((allTypes) => {
         if (EliminateScalarTypes(allTypes)) {
           if (ScalarAndEnumKindFinder(allTypes)) {
-            const typeName = gqlSchema.getType(allTypes.name) as GraphQLObjectType;
+            const typeName = gqlSchema.getType(
+              allTypes.name
+            ) as GraphQLObjectType;
             const fieldsInType = typeName.getFields();
             let fieldsArray: string[] = [];
             for (const type in fieldsInType) {
@@ -181,7 +184,12 @@ async function defineYourOwnApi(config: Config, templateDir: string) {
                 const node = gqlSchema.getType(
                   fieldsInType[type].type.inspect().replace(/[[\]!]/g, "")
                 )?.astNode;
-                if (node?.kind !== ("EnumTypeDefinition" || "UnionTypeDefinition" || "InputObjectTypeDefinition")) {
+                if (
+                  node?.kind !==
+                  ("EnumTypeDefinition" ||
+                    "UnionTypeDefinition" ||
+                    "InputObjectTypeDefinition")
+                ) {
                   if (schemaTypes.indexOf(type) === -1) {
                     schemaTypes.push(type);
                   }
@@ -193,16 +201,18 @@ async function defineYourOwnApi(config: Config, templateDir: string) {
           }
         }
       });
-      if(Object.keys(nestedResolverTypes).length <= 0){
-        stopSpinner(generatingCode, "nested resolvers are not possible with this schema normal resolvers are created", false);
-        model.api.nestedResolver = false
-      }else{
-        model.api.nestedResolverTypes = nestedResolverTypes
-        model.api.schemaTypes = schemaTypes
+      if (Object.keys(nestedResolverTypes).length <= 0) {
+        stopSpinner(
+          generatingCode,
+          "nested resolvers are not possible with this schema normal resolvers are created",
+          false
+        );
+        model.api.nestedResolver = false;
+      } else {
+        model.api.nestedResolverTypes = nestedResolverTypes;
+        model.api.schemaTypes = schemaTypes;
       }
     }
-
-    
 
     // Model Config
     const queriesFields: any = gqlSchema.getQueryType()?.getFields();
@@ -212,13 +222,12 @@ async function defineYourOwnApi(config: Config, templateDir: string) {
     model.api.queiresFields = [...Object.keys(queriesFields)];
     model.api.mutationFields = [...Object.keys(mutationsFields)];
     if (apiType === APITYPE.graphql) {
-      updatedPanacloudConfig = await generatePanacloudConfig(
-        model.api.queiresFields,
-        model.api.mutationFields,
-        model.api.architecture
-      );
+      updatedPanacloudConfig = await generatePanacloudConfig(model);
 
-      const mockApiCollection = buildSchemaToTypescript(gqlSchema, introspection);
+      const mockApiCollection = buildSchemaToTypescript(
+        gqlSchema,
+        introspection
+      );
       model.api.mockApiData = mockApiCollection;
     }
   } else {
