412876bb9e0107dafdb1d228c77085daecf55438
uzairbangee <uzairbangee@gmail.com>
2021-10-18 17:28:34 +0500

    feat: import changes

--- src/lib/api/generators/MockApi/index.ts
+++ src/lib/api/generators/MockApi/index.ts
@@ -11,46 +11,46 @@ class MockApiTestCollectionsFile {
   outputFile: string = `testCollections.ts`;
   outputDir: string = `types`;
   config: ApiModel;
-  code: CodeMaker;
 
   constructor(props: StackBuilderProps) {
     this.config = props.config;
-    this.code = new CodeMaker();
   }
 
   async mockApiTestCollectionsFile() {
-    const ts = new TypeScriptWriter(this.code);
+    const code = new CodeMaker();
+    const ts = new TypeScriptWriter(code);
 
-    this.code.openFile("testCollectionsTypes.ts");
+    code.openFile("testCollectionsTypes.ts");
 
     if (this.config.api.mockApiData?.imports) {
-      ts.writeImports("../../editable_src/graphql/types", [
+      ts.writeImports("./types", [
         ...this.config.api.mockApiData?.imports,
       ]);
     }
-    this.code.line();
-    this.code.indent(`export ${this.config.api.mockApiData?.types}`);
+    code.line();
+    code.indent(`export ${this.config.api.mockApiData?.types}`);
 
-    this.code.closeFile("testCollectionsTypes.ts");
-    await this.code.save(this.outputDir);
+    code.closeFile("testCollectionsTypes.ts");
+    await code.save(this.outputDir);
 
-    this.code.openFile(this.outputFile);
+    code.openFile(this.outputFile);
 
     ts.writeImports("./testCollectionsTypes", ["TestCollection"]);
+    console.log("this.config.api.mockApiData?.enumImports ", this.config.api.mockApiData)
     if (this.config.api.mockApiData?.enumImports.length !== 0) {
-      ts.writeImports("../../editable_src/graphql/types", [
+      ts.writeImports("./types", [
         ...this.config.api.mockApiData?.enumImports!,
       ]);
     }
-    this.code.line();
-    this.code.indent(`export const testCollections: TestCollection = {
+    code.line();
+    code.indent(`export const testCollections: TestCollection = {
         fields: 
         ${JSON.stringify(this.config.api.mockApiData?.collections.fields)} 
     }
     `);
 
-    this.code.closeFile(this.outputFile);
-    await this.code.save(this.outputDir);
+    code.closeFile(this.outputFile);
+    await code.save(this.outputDir);
 
     fse.copy("./types", "./lambdaLayer/mockApi").then(() => {
       fse.remove("./types").then(() => {});
