87d778fdbba7fc0e519d59d3dde30a66565d4e80
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2021-10-29 01:44:13 +0500

    getRandomItem helper added in apiMockDataGenerator

--- src/lib/api/apiMockDataGenerator/helper.ts
+++ src/lib/api/apiMockDataGenerator/helper.ts
@@ -1,3 +1,7 @@
 export function isArray(typeName: string) {
     return !!typeName.match(/[[a-zA-Z0-9]*]/g)
 }
+
+export function getRandomItem<T>(array: Array<T>) {
+    return array[Math.floor(Math.random() * array.length)]
+}
