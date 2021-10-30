e88f24fca377194b6d74dbeb3204e1201dfe87ac
Mateen Mustafa <66204034+mateen7861@users.noreply.github.com>
2021-10-30 21:08:28 +0500

    Update script.ts

--- test/test-scripts/script.ts
+++ test/test-scripts/script.ts
@@ -1,10 +1,19 @@
+import fs from "fs"
 const { execSync } = require("child_process");
 const expect = require("chai").expect;
+
 (() => {
   runCommand(`mkdir hello`)
   runCommand(`cd hello && ../bin/run init -t mateen7861`);
+  runCommand(`cd hello && npx gqlg --schemaFilePath ../test/test-schemas/todo.grapqhl --destDirPath ./tests/apiTests/`)
+  fs.writeFile("/hello/tests/apiTests/appsyncCredentials.json",{},(err)=>{
+    if(err){
+      process.exit(1)
+    }
+  })
   runCommand(`cd hello && yarn build`);
-  runCommand(`cd hello && cdk deploy`);
+  runCommand(`cd hello && cdk deploy \
+  --outputs-file ./tests/apiTests/appsyncCredentials.json`);
   execSync(`cd hello && yarn test`);
   execSync("cd hello && cdk destroy");
   execSync("rmdir hello");
