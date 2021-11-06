ea071027cc5dd8243a1e1decf06ca7f41bb9bc66
Muh-Hasan <muh.hasan.git@gmail.com>
2021-11-06 13:49:34 +0500

    @types/node & cdkTest commented (#51)

--- src/lib/api/generators/CdkTests/Appsync/index.ts
+++ src/lib/api/generators/CdkTests/Appsync/index.ts
@@ -1,144 +1,143 @@
-import { CodeMaker } from "codemaker";
-import {
-  CONSTRUCTS,
-  LAMBDASTYLE,
-  DATABASE,
-  ApiModel,
-  PanacloudconfigFile
-} from "../../../../../utils/constants";
-import { Cdk } from "../../../constructs/Cdk";
-import { Imports } from "../../../constructs/ConstructsImports";
-import { Iam } from "../../../constructs/Iam";
-import { Appsync } from "../../../constructs/Appsync";
-import { Lambda } from "../../../constructs/Lambda";
+// import { CodeMaker } from "codemaker";
+// import {
+//   CONSTRUCTS,
+//   DATABASE,
+//   ApiModel,
+//   PanacloudconfigFile
+// } from "../../../../../utils/constants";
+// import { Cdk } from "../../../constructs/Cdk";
+// import { Imports } from "../../../constructs/ConstructsImports";
+// import { Iam } from "../../../constructs/Iam";
+// import { Appsync } from "../../../constructs/Appsync";
+// import { Lambda } from "../../../constructs/Lambda";
 
-type StackBuilderProps = {
-  config: ApiModel;
-  panacloudConfig: PanacloudconfigFile
-};
-export class AppsyncApiConstructTest {
-  outputFile: string = `${CONSTRUCTS.appsync}.test.ts`;
-  outputDir: string = `test`;
-  config: ApiModel;
-  panacloudConfig: PanacloudconfigFile;
-  code: CodeMaker;
+// type StackBuilderProps = {
+//   config: ApiModel;
+//   panacloudConfig: PanacloudconfigFile
+// };
+// export class AppsyncApiConstructTest {
+//   outputFile: string = `${CONSTRUCTS.appsync}.test.ts`;
+//   outputDir: string = `test`;
+//   config: ApiModel;
+//   panacloudConfig: PanacloudconfigFile;
+//   code: CodeMaker;
 
-  constructor(props: StackBuilderProps) {
-    this.config = props.config;
-    this.panacloudConfig = props.panacloudConfig;
-    this.code = new CodeMaker();
-  }
+//   constructor(props: StackBuilderProps) {
+//     this.config = props.config;
+//     this.panacloudConfig = props.panacloudConfig;
+//     this.code = new CodeMaker();
+//   }
 
-  async AppsyncConstructTestFile() {
-    this.code.openFile(this.outputFile);
-    const {
-      api: {
-        apiName,
-        lambdaStyle,
-        database,
-        schema,
-        queiresFields,
-        mutationFields,
-      },
-    } = this.config;
-    const iam = new Iam(this.code);
-    const appsync = new Appsync(this.code);
-    const lambda = new Lambda(this.code, this.panacloudConfig);
-    const imp = new Imports(this.code);
-    const testClass = new Cdk(this.code);
-    const mutationsAndQueries = [...queiresFields!, ...mutationFields!];
-    imp.ImportsForTest();
-    imp.importForAppsyncConstructInTest();
-    imp.importForLambdaConstructInTest();
-    if (database === DATABASE.dynamoDB) {
-      imp.importForDynamodbConstructInTest();
-    } else if (database === DATABASE.neptuneDB) {
-      imp.importForNeptuneConstructInTest();
-    } else if (database === DATABASE.auroraDB) {
-      imp.importForAuroraDbConstructInTest();
-    }
-    this.code.line();
+//   async AppsyncConstructTestFile() {
+//     this.code.openFile(this.outputFile);
+//     const {
+//       api: {
+//         apiName,
+//         lambdaStyle,
+//         database,
+//         schema,
+//         queiresFields,
+//         mutationFields,
+//       },
+//     } = this.config;
+//     const iam = new Iam(this.code);
+//     const appsync = new Appsync(this.code);
+//     const lambda = new Lambda(this.code, this.panacloudConfig);
+//     const imp = new Imports(this.code);
+//     const testClass = new Cdk(this.code);
+//     const mutationsAndQueries = [...queiresFields!, ...mutationFields!];
+//     imp.ImportsForTest();
+//     imp.importForAppsyncConstructInTest();
+//     imp.importForLambdaConstructInTest();
+//     if (database === DATABASE.dynamoDB) {
+//       imp.importForDynamodbConstructInTest();
+//     } else if (database === DATABASE.neptuneDB) {
+//       imp.importForNeptuneConstructInTest();
+//     } else if (database === DATABASE.auroraDB) {
+//       imp.importForAuroraDbConstructInTest();
+//     }
+//     this.code.line();
 
-    testClass.initializeTest("Appsync Api Constructs Test", () => {
-      appsync.apiName = apiName;
+//     testClass.initializeTest("Appsync Api Constructs Test", () => {
+//       appsync.apiName = apiName;
 
-      if (database === DATABASE.dynamoDB) {
-        iam.constructorIdentifier(CONSTRUCTS.dynamoDB);
-      } else if (database === DATABASE.neptuneDB) {
-        iam.constructorIdentifier(CONSTRUCTS.neptuneDB);
-      } else if (database === DATABASE.auroraDB) {
-        iam.constructorIdentifier(CONSTRUCTS.auroraDB);
-      }
-      lambda.lambdaTestConstructInitializer(apiName, database, this.code);
-      appsync.appsyncTestConstructInitializer(
-        apiName,
-        lambdaStyle,
-        database,
-        mutationsAndQueries
-      );
-      this.code.line();
-      iam.appsyncApiTestIdentifier();
-      this.code.line();
-      appsync.appsyncApiTest();
-      this.code.line();
-      appsync.appsyncApiKeyTest();
-      this.code.line();
-      iam.appsyncRoleTestIdentifier();
-      this.code.line();
-      iam.appsyncServiceRoleTest();
-      this.code.line();
-      iam.appsyncRolePolicyTest();
-      this.code.line();
-      iam.lambdaConsturctIdentifier();
-      this.code.line();
-      iam.lambdaIdentifier();
-      this.code.line();
+//       if (database === DATABASE.dynamoDB) {
+//         iam.constructorIdentifier(CONSTRUCTS.dynamoDB);
+//       } else if (database === DATABASE.neptuneDB) {
+//         iam.constructorIdentifier(CONSTRUCTS.neptuneDB);
+//       } else if (database === DATABASE.auroraDB) {
+//         iam.constructorIdentifier(CONSTRUCTS.auroraDB);
+//       }
+//       lambda.lambdaTestConstructInitializer(apiName, database, this.code);
+//       appsync.appsyncTestConstructInitializer(
+//         apiName,
+//         lambdaStyle,
+//         database,
+//         mutationsAndQueries
+//       );
+//       this.code.line();
+//       iam.appsyncApiTestIdentifier();
+//       this.code.line();
+//       appsync.appsyncApiTest();
+//       this.code.line();
+//       appsync.appsyncApiKeyTest();
+//       this.code.line();
+//       iam.appsyncRoleTestIdentifier();
+//       this.code.line();
+//       iam.appsyncServiceRoleTest();
+//       this.code.line();
+//       iam.appsyncRolePolicyTest();
+//       this.code.line();
+//       iam.lambdaConsturctIdentifier();
+//       this.code.line();
+//       iam.lambdaIdentifier();
+//       this.code.line();
 
-      if (lambdaStyle === LAMBDASTYLE.single) {
-        let dsName = `${apiName}_dataSource`;
-        appsync.appsyncDatasourceTest(dsName, 0);
-        this.code.line();
-      } else if (lambdaStyle === LAMBDASTYLE.multi && mutationsAndQueries) {
-        mutationsAndQueries.forEach((key, index) => {
-          if (lambdaStyle === LAMBDASTYLE.multi) {
-            let dsName = `${apiName}_dataSource_${key}`;
-            appsync.appsyncDatasourceTest(dsName, index);
-            this.code.line();
-          }
-        });
-      }
+//       if (lambdaStyle === LAMBDASTYLE.single) {
+//         let dsName = `${apiName}_dataSource`;
+//         appsync.appsyncDatasourceTest(dsName, 0);
+//         this.code.line();
+//       } else if (lambdaStyle === LAMBDASTYLE.multi && mutationsAndQueries) {
+//         mutationsAndQueries.forEach((key, index) => {
+//           if (lambdaStyle === LAMBDASTYLE.multi) {
+//             let dsName = `${apiName}_dataSource_${key}`;
+//             appsync.appsyncDatasourceTest(dsName, index);
+//             this.code.line();
+//           }
+//         });
+//       }
 
-      queiresFields?.forEach((key) => {
-        if (lambdaStyle) {
-          let dsName = `${apiName}_dataSource`;
-          if (lambdaStyle === LAMBDASTYLE.multi) {
-            dsName = `${apiName}_dataSource_${key}`;
-          }
-          appsync.appsyncResolverTest(key, "Query", dsName);
-          this.code.line();
-        }
-      });
+//       queiresFields?.forEach((key) => {
+//         if (lambdaStyle) {
+//           let dsName = `${apiName}_dataSource`;
+//           if (lambdaStyle === LAMBDASTYLE.multi) {
+//             dsName = `${apiName}_dataSource_${key}`;
+//           }
+//           appsync.appsyncResolverTest(key, "Query", dsName);
+//           this.code.line();
+//         }
+//       });
 
-      mutationFields?.forEach((key) => {
-        if (lambdaStyle) {
-          let dsName = `${apiName}_dataSource`;
-          if (lambdaStyle === LAMBDASTYLE.multi) {
-            dsName = `${apiName}_dataSource_${key}`;
-          }
-          appsync.appsyncResolverTest(key, "Mutation", dsName);
-          this.code.line();
-        }
-      });
-    });
+//       mutationFields?.forEach((key) => {
+//         if (lambdaStyle) {
+//           let dsName = `${apiName}_dataSource`;
+//           if (lambdaStyle === LAMBDASTYLE.multi) {
+//             dsName = `${apiName}_dataSource_${key}`;
+//           }
+//           appsync.appsyncResolverTest(key, "Mutation", dsName);
+//           this.code.line();
+//         }
+//       });
+//     });
 
-    this.code.closeFile(this.outputFile);
-    await this.code.save(this.outputDir);
-  }
-}
+//     this.code.closeFile(this.outputFile);
+//     await this.code.save(this.outputDir);
+//   }
+// }
 
-export const AppsyncConstructTest = async (
-  props: StackBuilderProps
-): Promise<void> => {
-  const builder = new AppsyncApiConstructTest(props);
-  await builder.AppsyncConstructTestFile();
-};
+// export const AppsyncConstructTest = async (
+//   props: StackBuilderProps
+// ): Promise<void> => {
+//   const builder = new AppsyncApiConstructTest(props);
+//   await builder.AppsyncConstructTestFile();
+// };
--- src/lib/api/generators/CdkTests/Lambda/index.ts
+++ src/lib/api/generators/CdkTests/Lambda/index.ts
@@ -1,156 +1,156 @@
-import { CodeMaker } from "codemaker";
-import { TypeScriptWriter } from "../../../../../utils/typescriptWriter";
-import {
-  CONSTRUCTS,
-  APITYPE,
-  DATABASE,
-  LAMBDASTYLE,
-  PanacloudconfigFile,
-  ApiModel,
-} from "../../../../../utils/constants";
-import { Cdk } from "../../../constructs/Cdk";
-import { Imports } from "../../../constructs/ConstructsImports";
-import { Iam } from "../../../constructs/Iam";
-import { Lambda } from "../../../constructs/Lambda";
-import { neptuneIdentifierCalls, auroradbIdentifierCalls } from "./functions";
+// import { CodeMaker } from "codemaker";
+// import { TypeScriptWriter } from "../../../../../utils/typescriptWriter";
+// import {
+//   CONSTRUCTS,
+//   APITYPE,
+//   DATABASE,
+//   LAMBDASTYLE,
+//   PanacloudconfigFile,
+//   ApiModel,
+// } from "../../../../../utils/constants";
+// import { Cdk } from "../../../constructs/Cdk";
+// import { Imports } from "../../../constructs/ConstructsImports";
+// import { Iam } from "../../../constructs/Iam";
+// import { Lambda } from "../../../constructs/Lambda";
+// import { neptuneIdentifierCalls, auroradbIdentifierCalls } from "./functions";
 
-type StackBuilderProps = {
-  config: ApiModel;
-  panacloudConfig: PanacloudconfigFile
-};
+// type StackBuilderProps = {
+//   config: ApiModel;
+//   panacloudConfig: PanacloudconfigFile
+// };
 
-export class LambdaConstructTest {
-  outputFile: string = `${CONSTRUCTS.lambda}.test.ts`;
-  outputDir: string = `test`;
-  config: ApiModel;
-  panacloudConfig: PanacloudconfigFile;
-  code: CodeMaker;
+// export class LambdaConstructTest {
+//   outputFile: string = `${CONSTRUCTS.lambda}.test.ts`;
+//   outputDir: string = `test`;
+//   config: ApiModel;
+//   panacloudConfig: PanacloudconfigFile;
+//   code: CodeMaker;
 
-  constructor(props: StackBuilderProps) {
-    this.config = props.config;
-    this.panacloudConfig = props.panacloudConfig;
-    this.code = new CodeMaker();
-  }
+//   constructor(props: StackBuilderProps) {
+//     this.config = props.config;
+//     this.panacloudConfig = props.panacloudConfig;
+//     this.code = new CodeMaker();
+//   }
 
-  async construcLambdaConstructTestFile() {
-    const ts = new TypeScriptWriter(this.code);
-    this.code.openFile(this.outputFile);
+//   async construcLambdaConstructTestFile() {
+//     const ts = new TypeScriptWriter(this.code);
+//     this.code.openFile(this.outputFile);
 
-    const { apiName, lambdaStyle, database, apiType, schema } = this.config.api;
-    const cdk = new Cdk(this.code);
-    const lambda = new Lambda(this.code, this.panacloudConfig);
-    const imp = new Imports(this.code);
-    const iam = new Iam(this.code);
-    let mutationsAndQueries: string[] = [];
+//     const { apiName, lambdaStyle, database, apiType, schema } = this.config.api;
+//     const cdk = new Cdk(this.code);
+//     const lambda = new Lambda(this.code, this.panacloudConfig);
+//     const imp = new Imports(this.code);
+//     const iam = new Iam(this.code);
+//     let mutationsAndQueries: string[] = [];
 
-    if (apiType === APITYPE.graphql) {
-      const {
-        api: { mutationFields, queiresFields },
-      } = this.config;
-      mutationsAndQueries = [...queiresFields!, ...mutationFields!];
-    }
+//     if (apiType === APITYPE.graphql) {
+//       const {
+//         api: { mutationFields, queiresFields },
+//       } = this.config;
+//       mutationsAndQueries = [...queiresFields!, ...mutationFields!];
+//     }
 
-    imp.ImportsForTest();
-    imp.importForLambdaConstructInTest();
+//     imp.ImportsForTest();
+//     imp.importForLambdaConstructInTest();
 
-    if (database === DATABASE.dynamoDB) {
-      imp.importForDynamodbConstructInTest();
-    } else if (database === DATABASE.neptuneDB) {
-      imp.importForNeptuneConstructInTest();
-    } else if (database === DATABASE.auroraDB) {
-      imp.importForAuroraDbConstructInTest();
-    }
-    this.code.line();
-    cdk.initializeTest(
-      `Lambda Attach With ${
-        database === DATABASE.dynamoDB
-          ? "Dynamodb"
-          : database === DATABASE.neptuneDB
-          ? "Neptunedb"
-          : database === DATABASE.auroraDB
-          ? "Aurorodb"
-          : null
-      } Constructs Test`,
-      () => {
-        this.code.line();
-        if (database === DATABASE.dynamoDB) {
-          iam.constructorIdentifier(CONSTRUCTS.dynamoDB);
-        } else if (database === DATABASE.neptuneDB) {
-          iam.constructorIdentifier(CONSTRUCTS.neptuneDB);
-        } else if (database === DATABASE.auroraDB) {
-          iam.constructorIdentifier(CONSTRUCTS.auroraDB);
-        }
-        this.code.line();
-        lambda.lambdaTestConstructInitializer(apiName, database, this.code);
-        this.code.line();
-        if (database === DATABASE.dynamoDB) {
-          if (
-            apiType === APITYPE.rest ||
-            (lambdaStyle === LAMBDASTYLE.single && apiType === APITYPE.graphql)
-          ) {
-            let funcName = `${apiName}Lambda`;
-            this.code.line();
-            iam.DynodbTableTestIdentifier();
-            this.code.line();
-            lambda.initializeTestForLambdaWithDynamoDB(funcName, "main");
-            this.code.line();
-          } else if (lambdaStyle === LAMBDASTYLE.multi) {
-            this.code.line();
-            iam.DynodbTableTestIdentifier();
-            this.code.line();
-            mutationsAndQueries.forEach((key) => {
-              let funcName = `${apiName}Lambda${key}`;
-              lambda.initializeTestForLambdaWithDynamoDB(funcName, "index");
-              this.code.line();
-            });
-          }
-        } else if (database === DATABASE.neptuneDB) {
-          this.code.line();
-          neptuneIdentifierCalls(this.code);
-          if (
-            apiType === APITYPE.rest ||
-            (lambdaStyle === LAMBDASTYLE.single && apiType === APITYPE.graphql)
-          ) {
-            let funcName = `${apiName}Lambda`;
-            lambda.initializeTestForLambdaWithNeptune(funcName, "main");
-          } else if (lambdaStyle === LAMBDASTYLE.multi) {
-            mutationsAndQueries.forEach((key) => {
-              let funcName = `${apiName}Lambda${key}`;
-              lambda.initializeTestForLambdaWithNeptune(funcName, "index");
-              this.code.line();
-            });
-          }
-        } else if (database === DATABASE.auroraDB) {
-          this.code.line();
-          auroradbIdentifierCalls(this.code);
-          this.code.line();
-          if (
-            apiType === APITYPE.rest ||
-            (lambdaStyle === LAMBDASTYLE.single && apiType === APITYPE.graphql)
-          ) {
-            let funcName = `${apiName}Lambda`;
-            lambda.initializeTestForLambdaWithAuroradb(funcName, "main");
-          } else if (lambdaStyle === LAMBDASTYLE.multi) {
-            mutationsAndQueries.forEach((key) => {
-              let funcName = `${apiName}Lambda${key}`;
-              lambda.initializeTestForLambdaWithAuroradb(funcName, "index");
-              this.code.line();
-            });
-          }
-        }
-        iam.lambdaServiceRoleTest();
-        this.code.line();
-      }
-    );
+//     if (database === DATABASE.dynamoDB) {
+//       imp.importForDynamodbConstructInTest();
+//     } else if (database === DATABASE.neptuneDB) {
+//       imp.importForNeptuneConstructInTest();
+//     } else if (database === DATABASE.auroraDB) {
+//       imp.importForAuroraDbConstructInTest();
+//     }
+//     this.code.line();
+//     cdk.initializeTest(
+//       `Lambda Attach With ${
+//         database === DATABASE.dynamoDB
+//           ? "Dynamodb"
+//           : database === DATABASE.neptuneDB
+//           ? "Neptunedb"
+//           : database === DATABASE.auroraDB
+//           ? "Aurorodb"
+//           : null
+//       } Constructs Test`,
+//       () => {
+//         this.code.line();
+//         if (database === DATABASE.dynamoDB) {
+//           iam.constructorIdentifier(CONSTRUCTS.dynamoDB);
+//         } else if (database === DATABASE.neptuneDB) {
+//           iam.constructorIdentifier(CONSTRUCTS.neptuneDB);
+//         } else if (database === DATABASE.auroraDB) {
+//           iam.constructorIdentifier(CONSTRUCTS.auroraDB);
+//         }
+//         this.code.line();
+//         lambda.lambdaTestConstructInitializer(apiName, database, this.code);
+//         this.code.line();
+//         if (database === DATABASE.dynamoDB) {
+//           if (
+//             apiType === APITYPE.rest ||
+//             (lambdaStyle === LAMBDASTYLE.single && apiType === APITYPE.graphql)
+//           ) {
+//             let funcName = `${apiName}Lambda`;
+//             this.code.line();
+//             iam.DynodbTableTestIdentifier();
+//             this.code.line();
+//             lambda.initializeTestForLambdaWithDynamoDB(funcName, "main");
+//             this.code.line();
+//           } else if (lambdaStyle === LAMBDASTYLE.multi) {
+//             this.code.line();
+//             iam.DynodbTableTestIdentifier();
+//             this.code.line();
+//             mutationsAndQueries.forEach((key) => {
+//               let funcName = `${apiName}Lambda${key}`;
+//               lambda.initializeTestForLambdaWithDynamoDB(funcName, "index");
+//               this.code.line();
+//             });
+//           }
+//         } else if (database === DATABASE.neptuneDB) {
+//           this.code.line();
+//           neptuneIdentifierCalls(this.code);
+//           if (
+//             apiType === APITYPE.rest ||
+//             (lambdaStyle === LAMBDASTYLE.single && apiType === APITYPE.graphql)
+//           ) {
+//             let funcName = `${apiName}Lambda`;
+//             lambda.initializeTestForLambdaWithNeptune(funcName, "main");
+//           } else if (lambdaStyle === LAMBDASTYLE.multi) {
+//             mutationsAndQueries.forEach((key) => {
+//               let funcName = `${apiName}Lambda${key}`;
+//               lambda.initializeTestForLambdaWithNeptune(funcName, "index");
+//               this.code.line();
+//             });
+//           }
+//         } else if (database === DATABASE.auroraDB) {
+//           this.code.line();
+//           auroradbIdentifierCalls(this.code);
+//           this.code.line();
+//           if (
+//             apiType === APITYPE.rest ||
+//             (lambdaStyle === LAMBDASTYLE.single && apiType === APITYPE.graphql)
+//           ) {
+//             let funcName = `${apiName}Lambda`;
+//             lambda.initializeTestForLambdaWithAuroradb(funcName, "main");
+//           } else if (lambdaStyle === LAMBDASTYLE.multi) {
+//             mutationsAndQueries.forEach((key) => {
+//               let funcName = `${apiName}Lambda${key}`;
+//               lambda.initializeTestForLambdaWithAuroradb(funcName, "index");
+//               this.code.line();
+//             });
+//           }
+//         }
+//         iam.lambdaServiceRoleTest();
+//         this.code.line();
+//       }
+//     );
 
-    this.code.closeFile(this.outputFile);
-    await this.code.save(this.outputDir);
-  }
-}
+//     this.code.closeFile(this.outputFile);
+//     await this.code.save(this.outputDir);
+//   }
+// }
 
-export const lambdaConstructTest = async (
-  props: StackBuilderProps
-): Promise<void> => {
-  const builder = new LambdaConstructTest(props);
-  await builder.construcLambdaConstructTestFile();
-};
+// export const lambdaConstructTest = async (
+//   props: StackBuilderProps
+// ): Promise<void> => {
+//   const builder = new LambdaConstructTest(props);
+//   await builder.construcLambdaConstructTestFile();
+// };
