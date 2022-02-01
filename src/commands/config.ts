2a8a28ed4bdc05721c5540ead5eef85104a3cef4
Mateen Mustafa <66204034+mateen7861@users.noreply.github.com>
2022-02-02 00:06:54 +0500

    added command for updating panacloud config #158

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -16,16 +16,21 @@ export default class Config extends Command {
 
   static flags = {
     help: flags.help({ char: "h" }),
-    true: flags.boolean({ char: "t" }),
-    false: flags.boolean({ char: "f" }),
-    all: flags.boolean({ char: "a" }),
+    // true: flags.boolean({ char: "t" }),
+    // false: flags.boolean({ char: "f" }),
+    // all: flags.boolean({ char: "a" }),
+    mock:flags.string(),
+    memory:flags.integer(),
+    timeout:flags.integer()
   };
 
   async run() {
     const { flags, args } = this.parse(Config);
     const spinner = startSpinner("Updating Panacloud Config");
 
-    if (!flags.false && !flags.true && !args.queryName && !flags.all) {
+    if (
+      // !flags.false && !flags.true &&!flags.all&&
+       !args.queryName  && !flags.mock) {
       stopSpinner(
         spinner,
         "Please use panacloud config -h to get more info about the command",
@@ -37,70 +42,123 @@ export default class Config extends Command {
     const panacloudConfig: PanacloudconfigFile = readJsonSync(
       "editable_src/panacloudconfig.json"
     );
-
+    
     const keys = Object.keys(panacloudConfig.lambdas);
-
-    if (flags.all) {
-      if (flags.true) {
-        keys.forEach((e) => {
-          if (panacloudConfig.lambdas[e].is_mock === undefined) {
-            Object.keys(panacloudConfig.lambdas[e]).forEach(
-              (v) => (panacloudConfig.lambdas[e][v].is_mock = true)
-            );
-          } else {
-            panacloudConfig.lambdas[e].is_mock = true;
-          }
-        });
-      } else if (flags.false) {
-        keys.forEach((e) => {
-          if (panacloudConfig.lambdas[e].is_mock === undefined) {
-            Object.keys(panacloudConfig.lambdas[e]).forEach(
-              (v) => (panacloudConfig.lambdas[e][v].is_mock = false)
-            );
-          } else {
-            panacloudConfig.lambdas[e].is_mock = false;
-          }
-        });
-      } else {
-        stopSpinner(
-          spinner,
-          "panacloud config -a -t | --true  -f | --false",
-          true
-        );
-        process.exit(1);
-      }
-    }
-
     if (args.queryName) {
       if (!keys.includes(args.queryName)) {
-        this.log(`${args.queryName} not found`);
         stopSpinner(spinner, `${args.queryName} not found`, true);
         process.exit(1);
       }
-
-      if (flags.true) {
-        if (panacloudConfig.lambdas[args.queryName].is_mock) {
-          stopSpinner(spinner, `${args.queryName} already set to true`, true);
-          process.exit(1);
-        } else {
-          panacloudConfig.lambdas[args.queryName].is_mock = true;
-        }
-      } else if (flags.false) {
-        if (!panacloudConfig.lambdas[args.queryName].is_mock) {
-          stopSpinner(spinner, `${args.queryName} already set to false`, true);
-          process.exit(1);
-        } else {
-          panacloudConfig.lambdas[args.queryName].is_mock = false;
-        }
-      } else {
-        stopSpinner(
-          spinner,
-          "panacloud config query_name -t | --true  -f | --false",
-          true
-        );
-        process.exit(1);
-      }
     }
+      if(flags.mock){
+        if(flags.mock === "true"){
+          if (panacloudConfig.lambdas[args.queryName].is_mock) {
+            stopSpinner(spinner, `${args.queryName} already set to true`, true);
+            process.exit(1);
+          } else {
+            panacloudConfig.lambdas[args.queryName].is_mock = true;
+          }
+        }else if(flags.mock === "false"){
+          if (!panacloudConfig.lambdas[args.queryName].is_mock) {
+            stopSpinner(spinner, `${args.queryName} already set to false`, true);
+            process.exit(1);
+          } else {
+            panacloudConfig.lambdas[args.queryName].is_mock = false;
+          }
+        }else {
+          stopSpinner(
+            spinner,
+            "panacloud config query_name --mock true | false",
+            true
+          );
+          process.exit(1);
+        }
+      }
+      if(flags.memory){
+          if(flags.memory <= 0){
+            stopSpinner(
+              spinner,
+              "Invalid Memory Size",
+              true
+            );
+            process.exit(1);
+          }else{
+            panacloudConfig.lambdas[args.queryName].memory_size = flags.memory
+          }
+      }
+      if(flags.timeout){
+        if(flags.timeout <= 0){
+          stopSpinner(
+            spinner,
+            "Invalid Timeout",
+            true
+          );
+          process.exit(1);
+        }else{
+          panacloudConfig.lambdas[args.queryName].timeout = flags.timeout
+        }
+    }
+    // if (flags.all) {
+    //   if (flags.true) {
+    //     keys.forEach((e) => {
+    //       if (panacloudConfig.lambdas[e].is_mock === undefined) {
+    //         Object.keys(panacloudConfig.lambdas[e]).forEach(
+    //           (v) => (panacloudConfig.lambdas[e][v].is_mock = true)
+    //         );
+    //       } else {
+    //         panacloudConfig.lambdas[e].is_mock = true;
+    //       }
+    //     });
+    //   } else if (flags.false) {
+    //     keys.forEach((e) => {
+    //       if (panacloudConfig.lambdas[e].is_mock === undefined) {
+    //         Object.keys(panacloudConfig.lambdas[e]).forEach(
+    //           (v) => (panacloudConfig.lambdas[e][v].is_mock = false)
+    //         );
+    //       } else {
+    //         panacloudConfig.lambdas[e].is_mock = false;
+    //       }
+    //     });
+    //   } else {
+    //     stopSpinner(
+    //       spinner,
+    //       "panacloud config -a -t | --true  -f | --false",
+    //       true
+    //     );
+    //     process.exit(1);
+    //   }
+    // }
+
+    // if (args.queryName) {
+    //   if (!keys.includes(args.queryName)) {
+    //     this.log(`${args.queryName} not found`);
+    //     stopSpinner(spinner, `${args.queryName} not found`, true);
+    //     process.exit(1);
+    //   }
+
+    //   if (flags.true) {
+    //     if (panacloudConfig.lambdas[args.queryName].is_mock) {
+    //       stopSpinner(spinner, `${args.queryName} already set to true`, true);
+    //       process.exit(1);
+    //     } else {
+    //       panacloudConfig.lambdas[args.queryName].is_mock = true;
+    //     }
+    //   } else if (flags.false) {
+    //     if (!panacloudConfig.lambdas[args.queryName].is_mock) {
+    //       stopSpinner(spinner, `${args.queryName} already set to false`, true);
+    //       process.exit(1);
+    //     } else {
+    //       panacloudConfig.lambdas[args.queryName].is_mock = false;
+    //     }
+    //   } else {
+    //     stopSpinner(
+    //       spinner,
+    //       "panacloud config query_name -t | --true  -f | --false",
+    //       true
+    //     );
+    //     process.exit(1);
+    //   }
+    // }
 
     writeJsonSync("editable_src/panacloudconfig.json", panacloudConfig);
 
