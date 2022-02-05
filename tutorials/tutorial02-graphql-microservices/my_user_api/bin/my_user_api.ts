77c9605b7f46402b5ff75ea11c524d7e111e1878
Mateen Mustafa <66204034+mateen7861@users.noreply.github.com>
2022-02-05 19:05:45 +0500

    deleted project

--- tutorials/tutorial02-graphql-microservices/my_user_api/bin/my_user_api.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from "aws-cdk-lib";
-import { MyUserApiStack } from "../lib/my_user_api-stack";
-const app: cdk.App = new cdk.App();
-const deployEnv = process.env.STAGE;
-const stack = new MyUserApiStack(
-  app,
-  deployEnv ? deployEnv + "-MyUserApiStack" : "MyUserApiStack",
-  { prod: deployEnv }
-);
