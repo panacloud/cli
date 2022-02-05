7990291b9cd3415df430d8f1e4a5cbbc3a17db24
Mateen Mustafa <66204034+mateen7861@users.noreply.github.com>
2022-02-05 18:59:21 +0500

    Initialized tutorial 2 with lattest cli and made some changes

--- tutorials/tutorial01-custom-mock-api/editable_src/customMockLambdaLayer/mockData/addUser/testCollectionsTypes.ts
+++
@@ -1,5 +0,0 @@
-import { User, MutationAddUserArgs, QueryUserArgs } from "../../../../types";
-
-export type TestCollection = {
-  fields: { addUser: { arguments: MutationAddUserArgs; response: User }[] };
-};
