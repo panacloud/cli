9188f36f6c9614b1fab5b6844d046c835eef10ba
Mateen Mustafa <66204034+mateen7861@users.noreply.github.com>
2022-02-06 15:01:35 +0500

    added turorial 4 project

---
+++ tutorials/tutorial04-customizing-infrastructure-using-aspects/my_user_api/mock_lambda_layer/mockData/addUser/testCollections.ts
@@ -0,0 +1,17 @@
+import { TestCollection } from "./testCollectionsTypes";
+
+export const testCollections: TestCollection = {
+  fields: {
+    addUser: [
+      {
+        arguments: {
+          name: "Elladine",
+        },
+        response: {
+          id: "Kial",
+          name: "Myrtie",
+        },
+      },
+    ],
+  },
+};
