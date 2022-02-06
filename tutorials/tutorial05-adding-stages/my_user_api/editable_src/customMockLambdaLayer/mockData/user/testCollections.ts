99466c5f7b3d9d7979105b7f7ea993a2719ce311
Mateen Mustafa <66204034+mateen7861@users.noreply.github.com>
2022-02-06 15:29:01 +0500

    added project in tutorial 5

---
+++ tutorials/tutorial05-adding-stages/my_user_api/editable_src/customMockLambdaLayer/mockData/user/testCollections.ts
@@ -0,0 +1,17 @@
+import { TestCollection } from "./testCollectionsTypes";
+
+export const testCollections: TestCollection = {
+  fields: {
+    user: [
+      {
+        arguments: {
+          id: "Katerina",
+        },
+        response: {
+          id: "Gertrude",
+          name: "Gayle",
+        },
+      },
+    ],
+  },
+};
---
+++ tutorials/tutorial05-adding-stages/my_user_api/mock_lambda_layer/mockData/addUser/testCollections.ts
@@ -0,0 +1,17 @@
+import { TestCollection } from "./testCollectionsTypes";
+
+export const testCollections: TestCollection = {
+  fields: {
+    addUser: [
+      {
+        arguments: {
+          name: "Kylynn",
+        },
+        response: {
+          id: "Coralyn",
+          name: "Kamilah",
+        },
+      },
+    ],
+  },
+};
